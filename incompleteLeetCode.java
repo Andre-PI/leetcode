862ed7c9464bfeac9414296ccbe8558bfcbbd130
Andre Lucas <141793916+Andre-PI@users.noreply.github.com>
2024-03-23 22:19:29 -0300

    Update incompleteLeetCode.java

--- incompleteLeetCode.java
+++ incompleteLeetCode.java
@@ -1,46 +1,68 @@
+/*20. Valid Parentheses
+leetcode link: https://leetcode.com/problems/valid-parentheses/description/
+Solved
+Easy
+Topics
+Companies
+Hint
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+
+
+Accepted
+Solution
+Runtime 1ms
+Beats 98.66% of users with Java
+
+Memory
+41.43 MB 
+Beats
+46.12%
+of users with Java
+*/
 class Solution {
-    private boolean checkOtherPart(String str){
-        switch(str){
-                case "(":
-                    otherPart = ")";
-                    if(loop(i, array,otherPart) == false){
-                        return false;
-                    }else{
-                        continue;
-                    }
-                case "[":
-                    otherPart = "]";
-                    if(loop(i, array,otherPart) == false){
-                        return false;
-                    }else{
-                        continue;
-                    }
-                case "{":
-                    otherPart = "}";
-                    if(loop(i, array,otherPart) == false){
-                        return false;
-                    }else{
-                        continue;
-                    }
-            }
-    }
-    private boolean loop(String s,int count, String[] array, String otherPart){
-            for(int e = count + 1; e < array.length;e++){
-                if(array[e].equals(otherPart)){
-                    return true;
-                }else if(array[e].equals("(") || array[e].equals("[") || array[e].equals("{")){
-                    
+ 
+    Stack<Character> stack = new Stack<>();
+    public boolean isValid(String s) {
+        for (char value : s.toCharArray()) {
+            if (value == '(' || value == '[' || value == '{') {
+                stack.push(value);
+            } else {
+                if (stack.empty() ||
+                        value == ')' && stack.peek() != '(' ||
+                        value == ']' && stack.peek() != '[' ||
+                        value == '}' && stack.peek() != '{') {
+                    return false;
+                } else {
+                    stack.pop();
                 }
             }
-            return false;
-    }
-    public boolean isValid(String s) {
-        String[] array = new String[s.split("").length];
-        array = s.split("");
-        String otherPart = "";
-        for(int i = 0;i<array.length; i++){
-            checkOtherPart(array[i]);
         }
-        return true;
+        return stack.empty();
     }
 }
